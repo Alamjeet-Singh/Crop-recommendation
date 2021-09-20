58ba3e70dbcfbb31ea8c31b467146386d08c00d3
Alamjeet Singh <69529536+Alamjeet-Singh@users.noreply.github.com>
2021-09-20 22:32:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask ,render_template,url_for,request,flash,redirect
 import pickle
 import pandas as pd
-import sklearn
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.secret_key="."
